4493efa14adff6c63cf9e83b1e4ccaa19bbe70d3
Gavin Fitzgerald <gavin.fitzgerald@bearingpoint.com>
2019-08-14 17:34:34 +0100

    Another commit agen

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.pluralsight.controller;
 
+import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MinutesController {
@@ -11,7 +17,27 @@ public class MinutesController {
     @RequestMapping(value = "/addMinutes")
     public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
         System.out.println("exercise: " + exercise.getMinutes());
+        System.out.println("exercise activity: " + exercise.getActivity());
         return "addMinutes";
+    }
+
+    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+    public @ResponseBody List<Activity> findAllActivities(){
+        List<Activity> activities = new ArrayList<>();
+
+        Activity run = new Activity();
+        run.setDesc("Run");
+        activities.add(run);
+
+        Activity bike = new Activity();
+        bike.setDesc("bike");
+        activities.add(bike);
+
+        Activity swim = new Activity();
+        swim.setDesc("swim");
+        activities.add(swim);
+
+        return activities;
     }
 
 //    @RequestMapping(value = "/addMoreMinutes")
