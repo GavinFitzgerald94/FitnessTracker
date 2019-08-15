6fb56bf636ee2ec85d532048bf3e662885d09481
Gavin Fitzgerald <gavin.fitzgerald@bearingpoint.com>
2019-08-15 17:31:01 +0100

    Another commit again

--- src/main/java/com/pluralsight/service/ExerciseServiceImpl.java
+++ src/main/java/com/pluralsight/service/ExerciseServiceImpl.java
@@ -1,7 +1,31 @@
 package com.pluralsight.service;
 
+import com.pluralsight.model.Activity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service("exerciseService")
-public class ExerciseServiceImpl {
+public class ExerciseServiceImpl implements ExerciseService {
+
+    @Override
+    public List<Activity> findAllActivities(){
+        List<Activity> activities = new ArrayList<>();
+
+        Activity run = new Activity();
+        run.setDesc("run");
+        activities.add(run);
+
+        Activity bike = new Activity();
+        bike.setDesc("bike");
+        activities.add(bike);
+
+        Activity swim = new Activity();
+        swim.setDesc("swim");
+        activities.add(swim);
+
+        return activities;
+    }
+
 }
